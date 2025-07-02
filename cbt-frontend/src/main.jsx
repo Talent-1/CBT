b33e245783f19c83bb1afa9cbff4ea891015a714
Talent-1 <itzmhiztatalent@gmail.com>
2025-07-02 23:37:50 +0100

    updated fontawesome for social handles display

--- cbt-frontend/src/main.jsx
+++ cbt-frontend/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './hooks/useAuth'; // Make sure AuthProvider wraps App
 import './index.css'; // Your global styles
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
